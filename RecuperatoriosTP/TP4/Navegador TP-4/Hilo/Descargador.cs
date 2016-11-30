8e591022c051f5f19214a3d84ecf4b8f9fd1327e
ddee1 <ddee1@outlook.es>
2016-11-30 01:20:38 -0300

    Add files via upload

---
+++ RecuperatoriosTP/TP4/Navegador TP-4/Hilo/Descargador.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        //private string html;
+        private Uri direccion;
+        private string result;
+
+        public delegate void Progress(int progreso);
+        public delegate void DownloadComplete(string html);
+
+        public event Progress progress;
+        public event DownloadComplete downloadComplete;
+
+        public Descargador(Uri direccion)
+        {
+            this.direccion = direccion;
+            //this.html = "http://"+direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += WebClientDownloadProgressChanged;
+                cliente.DownloadStringCompleted += WebClientDownloadCompleted;
+                cliente.DownloadStringAsync(direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.progress(e.ProgressPercentage);
+        }
+
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            this.result = e.Result;
+            this.downloadComplete(this.result);
+        }
+    }
+}
